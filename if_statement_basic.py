2f48096210cf53597acb3607521a9c90aae6402e
Hot00Sauce <johnrollycedillo15@gmail.com>
2025-02-15 09:27:50 +0800

    Submitting Python Week 04 - Laboratory # 08 by John Rolly N. Cedillo

---
+++ if_statement_basic.py
@@ -0,0 +1,9 @@
+temperature = 35  # Or any other number
+
+if temperature > 30:
+
+    print("Ang hot ko!")  # Indented 4 spaces
+
+#elif temperature < 30: 
+
+    #print("Ang lamig!!")  # Indented 4 spaces
